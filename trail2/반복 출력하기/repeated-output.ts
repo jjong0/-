a9982ae4d58f6c10b522fe9c3b72dc751bc7f430
jjong0 <ujjong39@gmail.com>
2026-09-18 14:21:13 +0900

    [Passed] 반복 출력하기 (75ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.ts
@@ -0,0 +1,9 @@
+function printNLines(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("12345^&*()_");
+    }
+}
+
+const fs = require("fs");
+let rowNum = Number(fs.readFileSync(0).toString().trim());
+printNLines(rowNum);
